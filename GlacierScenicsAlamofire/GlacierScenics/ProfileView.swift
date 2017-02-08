09b9f2c3a1b296e3a3cada67bdf5c5d3250e73ae
ant.man <ant.man@kakaocorp.com>
2017-02-09 07:53:10 +0900

    add following & follower cnt in profile view

--- GlacierScenicsAlamofire/GlacierScenics/ProfileView.swift
+++ GlacierScenicsAlamofire/GlacierScenics/ProfileView.swift
@@ -7,19 +7,52 @@
 //
 
 import UIKit
+import Alamofire
 
 class ProfileView: UIViewController {
 
+    @IBOutlet weak var followingCntLabel: UILabel!
+    @IBOutlet weak var followerCntLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        loadUser()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func loadUser() {
+        // https://test.owltree.us/users/U158564A0AF0US2?access_token=9cd741d9-10a2-4bc7-955c-3dc1b2ddf60b
+        let url = "https://test.owltree.us/users/U158564A0AF0US2"
+        
+        let parameters: Parameters = [
+            "access_token": "9cd741d9-10a2-4bc7-955c-3dc1b2ddf60b"
+        ]
+        
+        Alamofire.request(url, method: .get, parameters: parameters).responseObject { (response: DataResponse<User>) in
+            
+            guard response.result.isSuccess else {
+                print("Error while request : \(response.result.error)")
+                return
+            }
+            
+            let user = response.result.value
+            
+            print ((user?.followingNum ?? 0))
+            self.followingCntLabel.text = String(user?.followingNum ?? 0)
+            self.followerCntLabel.text = String(user?.followerNum ?? 0)
+            
+            
+            self.followerCntLabel.reloadInputViews()
+            self.followingCntLabel.reloadInputViews()
+        }
+    }
+
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if ( segue.identifier == "follower" || segue.identifier == "following" ) {
--- GlacierScenicsAlamofire/GlacierScenics/User.swift
+++ GlacierScenicsAlamofire/GlacierScenics/User.swift
@@ -10,17 +10,23 @@ class User : NSObject, NSCoding, Mappable{
     
     var bigPicture : String?
     var bio : String?
-    var birthyear : AnyObject?
+    var birthyear : Int?
     var country : String?
+    var facebookId : String?
     var firebaseYN : AnyObject?
+    var followerNum : Int?
+    var followingNum : Int?
     var gmtOffsetSec : Int?
     var instagramId : String?
+    var instagramName : String?
     var languages : String?
     var name : String?
     var oauthAccessToken : AnyObject?
+    var owlName : String?
     var picture : String?
-    var sex : AnyObject?
-    var status : AnyObject?
+    var realName : AnyObject?
+    var sex : String?
+    var status : String?
     var userId : String?
     
     
@@ -36,13 +42,19 @@ class User : NSObject, NSCoding, Mappable{
         bio <- map["bio"]
         birthyear <- map["birthyear"]
         country <- map["country"]
+        facebookId <- map["facebookId"]
         firebaseYN <- map["firebaseYN"]
+        followerNum <- map["followerNum"]
+        followingNum <- map["followingNum"]
         gmtOffsetSec <- map["gmtOffsetSec"]
         instagramId <- map["instagramId"]
+        instagramName <- map["instagramName"]
         languages <- map["languages"]
         name <- map["name"]
         oauthAccessToken <- map["oauthAccessToken"]
+        owlName <- map["owlName"]
         picture <- map["picture"]
+        realName <- map["realName"]
         sex <- map["sex"]
         status <- map["status"]
         userId <- map["userId"]
@@ -57,17 +69,23 @@ class User : NSObject, NSCoding, Mappable{
     {
         bigPicture = aDecoder.decodeObject(forKey: "bigPicture") as? String
         bio = aDecoder.decodeObject(forKey: "bio") as? String
-        birthyear = aDecoder.decodeObject(forKey: "birthyear") as? AnyObject
+        birthyear = aDecoder.decodeObject(forKey: "birthyear") as? Int
         country = aDecoder.decodeObject(forKey: "country") as? String
+        facebookId = aDecoder.decodeObject(forKey: "facebookId") as? String
         firebaseYN = aDecoder.decodeObject(forKey: "firebaseYN") as? AnyObject
+        followerNum = aDecoder.decodeObject(forKey: "followerNum") as? Int
+        followingNum = aDecoder.decodeObject(forKey: "followingNum") as? Int
         gmtOffsetSec = aDecoder.decodeObject(forKey: "gmtOffsetSec") as? Int
         instagramId = aDecoder.decodeObject(forKey: "instagramId") as? String
+        instagramName = aDecoder.decodeObject(forKey: "instagramName") as? String
         languages = aDecoder.decodeObject(forKey: "languages") as? String
         name = aDecoder.decodeObject(forKey: "name") as? String
         oauthAccessToken = aDecoder.decodeObject(forKey: "oauthAccessToken") as? AnyObject
+        owlName = aDecoder.decodeObject(forKey: "owlName") as? String
         picture = aDecoder.decodeObject(forKey: "picture") as? String
-        sex = aDecoder.decodeObject(forKey: "sex") as? AnyObject
-        status = aDecoder.decodeObject(forKey: "status") as? AnyObject
+        realName = aDecoder.decodeObject(forKey: "realName") as? AnyObject
+        sex = aDecoder.decodeObject(forKey: "sex") as? String
+        status = aDecoder.decodeObject(forKey: "status") as? String
         userId = aDecoder.decodeObject(forKey: "userId") as? String
         
     }
@@ -90,14 +108,26 @@ class User : NSObject, NSCoding, Mappable{
         if country != nil{
             aCoder.encode(country, forKey: "country")
         }
+        if facebookId != nil{
+            aCoder.encode(facebookId, forKey: "facebookId")
+        }
         if firebaseYN != nil{
             aCoder.encode(firebaseYN, forKey: "firebaseYN")
+        }
+        if followerNum != nil{
+            aCoder.encode(followerNum, forKey: "followerNum")
+        }
+        if followingNum != nil{
+            aCoder.encode(followingNum, forKey: "followingNum")
         }
         if gmtOffsetSec != nil{
             aCoder.encode(gmtOffsetSec, forKey: "gmtOffsetSec")
         }
         if instagramId != nil{
             aCoder.encode(instagramId, forKey: "instagramId")
+        }
+        if instagramName != nil{
+            aCoder.encode(instagramName, forKey: "instagramName")
         }
         if languages != nil{
             aCoder.encode(languages, forKey: "languages")
@@ -108,8 +138,14 @@ class User : NSObject, NSCoding, Mappable{
         if oauthAccessToken != nil{
             aCoder.encode(oauthAccessToken, forKey: "oauthAccessToken")
         }
+        if owlName != nil{
+            aCoder.encode(owlName, forKey: "owlName")
+        }
         if picture != nil{
             aCoder.encode(picture, forKey: "picture")
+        }
+        if realName != nil{
+            aCoder.encode(realName, forKey: "realName")
         }
         if sex != nil{
             aCoder.encode(sex, forKey: "sex")
